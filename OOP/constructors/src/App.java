21b439460ab881a7e1da60f6b6a816b640001a1e
Rendy Syahputra <rendysyhptra2057@gmail.com>
2026-01-31 17:15:51 +0700

    learning: complete constructor (OOP)

--- OOP/constructors/src/App.java
+++ OOP/constructors/src/App.java
@@ -1,5 +1,21 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        Student student1 = new Student("John Marston", 30, 3.8);
+        Student student2 = new Student("Jack Marston", 20, 4.0);
+
+        System.out.println(student1.name);
+        System.out.println(student1.age);
+        System.out.println(student1.gpa);
+        System.out.println(student1.isEnrolled);
+
+        System.out.println(student2.name);
+        System.out.println(student2.age);
+        System.out.println(student2.gpa);
+        System.out.println(student2.isEnrolled);
+
+        student1.study();
+        student2.study();
+        
     }
 }
--- OOP/constructors/src/Student.java
+++ OOP/constructors/src/Student.java
@@ -1,3 +1,19 @@
 public class Student {
 
+    String name;
+    int age;
+    double gpa;
+    boolean isEnrolled;
+
+    Student(String name, int age, double gpa){
+        this.name = name;
+        this.age = age;
+        this.gpa = gpa;
+        this.isEnrolled = true;
+    }
+
+    void study(){
+        System.out.printf("%s is studying!\n", this.name);
+    }
+    
 }
