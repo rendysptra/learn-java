a03f84e55bbb9877c0c20dd4c77bbb0b093120f1
Rendy Syahputra <rendysyhptra2057@gmail.com>
2026-02-01 17:02:47 +0700

    learning: complete toString Method (OOP)

--- OOP/toStringMethod/src/App.java
+++ OOP/toStringMethod/src/App.java
@@ -1,5 +1,9 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        Car car = new Car("Ford", "Mustang", 2025, "Black");
+
+        System.out.println(car);
+        
     }
 }
