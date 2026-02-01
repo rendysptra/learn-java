f174eb5ad4aa213af1d5dcccd6b7fa97a628a776
Rendy Syahputra <rendysyhptra2057@gmail.com>
2026-02-01 14:12:40 +0700

    learning: complete static (OOP)

--- OOP/static/src/App.java
+++ OOP/static/src/App.java
@@ -1,5 +1,14 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        Friend friend1 = new Friend("Benoit Blanc");
+        Friend friend2 = new Friend("Myles Bron");
+        Friend friend3 = new Friend("Duke");
+        Friend friend4 = new Friend("Birdie");
+
+        // System.out.println(Friend.numOfFriend);
+
+        Friend.showFriend();
+        
     }
 }
--- OOP/static/src/Friend.java
+++ OOP/static/src/Friend.java
@@ -1,3 +1,14 @@
 public class Friend {
     
+    static int numOfFriend;
+    String name;
+
+    Friend(String name){
+        this.name = name;
+        numOfFriend++;
+    }
+
+    static void showFriend(){
+        System.out.println("You have " + numOfFriend + " total friends");
+    }
 }
