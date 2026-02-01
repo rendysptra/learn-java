fae20f3aaa2a5aef5edd7f705b901c4c507ad229
Rendy Syahputra <rendysyhptra2057@gmail.com>
2026-02-01 13:51:57 +0700

    learning: complete array of object (OOP)

--- OOP/arrayOfObjects /src/App.java
+++ OOP/arrayOfObjects /src/App.java
@@ -1,5 +1,23 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        // Car car1 = new Car("Mustang",  "Red");
+        // Car car2 =  new Car("Corvette", "Blue");
+        // Car car3 = new Car("Charger", "Black");
+
+        Car[] cars = {
+            new Car("Mustang", "Red"),
+            new Car("Corvette", "Blue"),
+            new Car("Charger", "Black")
+        };
+
+        for(Car car : cars){
+            car.color = "Black";
+        }
+
+        for(Car car : cars){
+            System.out.println(car.color + " " + car.model);
+        }
+        
     }
 }
--- OOP/arrayOfObjects /src/Car.java
+++ OOP/arrayOfObjects /src/Car.java
@@ -1,3 +1,15 @@
 public class Car {
 
+    String model;
+    String color;
+
+    Car(String model, String color){
+        this.model = model;
+        this.color = color;
+    }
+
+    void drive(){
+        System.out.printf("You drive the %s %s\n", this.color, this.model);
+    }
+    
 }
