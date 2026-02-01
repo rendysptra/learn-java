806424a4bcbf418a22ddd06696e811224f5d83a3
Rendy Syahputra <rendysyhptra2057@gmail.com>
2026-02-01 13:29:06 +0700

    learning: completed overloaded constructor (OOP)

--- OOP/overloadedConstructors/src/App.java
+++ OOP/overloadedConstructors/src/App.java
@@ -1,5 +1,27 @@
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        User user1 = new User();
+        User user2 = new User("benoitBlanc");
+        User user3 = new User("martaCabrera", "martacbrra@gmail.com");
+        User user4 = new User("harlanThrombey", "thrombeyH@gmail.com", 80);
+
+        System.out.println(user1.username);
+        System.out.println(user1.email);
+        System.out.println(user1.age);
+
+        System.out.println(user2.username);
+        System.out.println(user2.email);
+        System.out.println(user2.age);
+
+        System.out.println(user3.username);
+        System.out.println(user3.email);
+        System.out.println(user3.age);
+
+        System.out.println(user4.username);
+        System.out.println(user4.email);
+        System.out.println(user4.age);
+        
     }
 }
--- OOP/overloadedConstructors/src/User.java
+++ OOP/overloadedConstructors/src/User.java
@@ -1,3 +1,31 @@
 public class User {
 
+    String username;
+    String email;
+    int age;
+
+    User(){
+        this.username = "Guest";
+        this.email = "No email";
+        this.age = 0;
+    }
+
+    User(String username){
+        this.username = username;
+        this.email = "No email";
+        this.age = 0;
+    }
+
+    User(String username, String email){
+        this.username = username;
+        this.email = email;
+        this.age = 0;
+    }
+
+    User(String username, String email, int age){
+        this.username = username;
+        this.email = email;
+        this.age = age;
+    }
+    
 }
